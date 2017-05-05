347aa681527e952eeb581724e63c024bbb696208
Edison Pizarro <edison.pl@hotmail.com>
2017-05-04 22:09:40 -0300

    Updated Test4

--- index.js
+++ index.js
@@ -267,7 +267,7 @@ io.on('connection', function(socket){
 
 /**** Bootup the Server ****/
 
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 8000;
 server.listen(port, function(err){
     if(err) throw err;
     console.log("Server now running at port: "+port);

f72ae14b1767bdbe236ab6af2be251ad0a02a4b7
Edison Pizarro <edison.pl@hotmail.com>
2017-05-04 21:55:57 -0300

    Updated Test

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
-var http = require('http');
-var server = http.createServer();
-function control(petic, resp) {
-resp.writeHead(200, {'content-type': 'text/plain'});
-resp.write('Hola, Mundo!');
-resp.end();
+//Importamos Express tras instalarlo vía NPM
+import express from 'express'
+
+// Definimos App como la función del módulo Express
+const App = express()
+
+// Definimos algunas variables que usaremos en las distintas funciones
+const port = 3000  
+const options = {  
+  root: __dirname
 }
-server.on('request', control);
-server.listen(8080);
+
+// Definimos funciones para luego usarlas al recibir una petición en el router
+function getHTML(req, res) {  
+  res.sendFile('./index.html', options, (err) => {
+    if (err) throw err;
+    console.log('Sirviendo index.html')
+  })
+}
+
+// Definimos las rutas
+App.get('/', getHTML)
+
+// Escuchamos el puerto de Express
+App.listen(port, () => {  
+  console.log('Aplicacion escuchando en el puerto: ' + port)
+})
